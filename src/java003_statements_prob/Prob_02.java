003b1d55d89c0bfcab00c4ea2338c2c887f5bd1e
eoooomji <akcks101@gmail.com>
2022-10-07 18:19:57 +0900

    update | 20221007

--- src/java003_statements_prob/Prob_02.java
+++ src/java003_statements_prob/Prob_02.java
@@ -12,6 +12,12 @@ public class Prob_02 {
 
 	public static void main(String[] args) {
 		
+		for(int year = 2001; year <= 2012 ; year++) {
+			if((year%4 == 0 && year% 100 !=0) || (year % 400 == 0) ) {
+				System.out.println(year);
+			}
+		}
+		
       
        
 	}//end main()
--- src/java003_statements_prob/Prob_03.java
+++ src/java003_statements_prob/Prob_03.java
@@ -13,7 +13,10 @@ sum=35
 public class Prob_03 {
 
 	public static void main(String[] args) {
-		
+		int sum = 0;
+		for (int i = 1; i <= 5; i++) {
+			sum += i;
+		}System.out.printf("sum=%d \n",sum);
 	}// end main()
 
 }// end class
