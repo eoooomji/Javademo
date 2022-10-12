bc8458ca52a7ad2ae8e0c729805d635c06946e81
eoooomji <akcks101@gmail.com>
2022-10-12 17:29:55 +0900

    update | 20221012

--- src/java005_method/prob/Prob007_method.java
+++ src/java005_method/prob/Prob007_method.java
@@ -36,30 +36,31 @@ public class Prob007_method {
 
 	private static int[] sort(int[] array, String orderby) {
 		// 구현하시오.
-		int[] sorted_Arr = array.clone();
 		int temp;
+		int[] sorted_Arr = array.clone();
 		
-		
-		if(orderby=="desc") {
-			for(int i = 0 ; i<array.length; i++) { //반복횟수
-			  for(int j =1; j<sorted_Arr.length; j++) { //bubble 정렬
-				  if(sorted_Arr[j-1] < sorted_Arr[j] ) {
-					  temp = sorted_Arr[j-1];
-					  sorted_Arr[j-1] = sorted_Arr[j];
-					  sorted_Arr[j] = temp;
-				  }
-			  }
-			}
-		}else {
-			for(int i = 0 ; i<array.length; i++) { //반복횟수
-				  for(int j =1; j<sorted_Arr.length; j++) { //정렬
-					  if(sorted_Arr[j-1] > sorted_Arr[j] ) {
-						  temp = sorted_Arr[j-1];
-						  sorted_Arr[j-1] = sorted_Arr[j];
-						  sorted_Arr[j] = temp;
-					  }
-				  }
+		if(orderby.equals("desc")) {
+			for(int i = 0; i < sorted_Arr.length ; i++) {
+				for(int j = 1; j<sorted_Arr.length ; j++) {
+					if(sorted_Arr[j-1]<sorted_Arr[j]) {
+						temp = sorted_Arr[j-1];
+						sorted_Arr[j-1] = sorted_Arr[j];
+						sorted_Arr[j]= temp;
+					}
 				}
+			}
+		}
+		
+		if(orderby.equals("asc")) {
+			for(int i =0; i<sorted_Arr.length ; i++) {
+				for(int j =1;j<sorted_Arr.length;j++) {
+					if(sorted_Arr[j-1]>sorted_Arr[j]) {
+						temp = sorted_Arr[j-1];
+						sorted_Arr[j-1]=sorted_Arr[j];
+						sorted_Arr[j] = temp;
+					}
+				}
+			}
 		}
 		return sorted_Arr;
 	}//end sort( )
