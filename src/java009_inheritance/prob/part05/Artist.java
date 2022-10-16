36d5f6a2dd4e7086657cf181984e8f1f58597571
EomJiChan <akcks101@gmail.com>
2022-10-16 19:53:24 +0900

    update | 20221016

---
+++ src/java009_inheritance/prob/part05/Artist.java
@@ -0,0 +1,16 @@
+package java009_inheritance.prob.part05;
+
+public class Artist {
+	String artist; // 아티스트
+	String enter; // 소속사
+	
+	public Artist() {
+		
+	}
+
+	public Artist(String artist, String enter) {
+		this.artist = artist;
+		this.enter = enter;
+	}
+	
+}
