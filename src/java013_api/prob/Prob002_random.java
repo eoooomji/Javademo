a930fa3acb377a5354453f7c86ddb63673e75ebd
eoooomji <akcks101@gmail.com>
2022-10-20 18:17:38 +0900

    update | 20221020

--- src/java013_api/prob/Prob002_random.java
+++ src/java013_api/prob/Prob002_random.java
@@ -30,25 +30,29 @@ public class Prob002_random {
 
 	private static int[] makeArray() {
 		// 난수값를 발생시킨후 반환하는 프로그램을 구현하시오.
-		
-		return null;
+		int[] arr = new int[10];
+		Random ran = new Random();
+
+		for (int i = 0; i < arr.length; i++)
+			arr[i] = ran.nextInt(20) + 1; // nextInt(20) : 0이상 20미만의 범위
+
+		return arr;
 	}// end makeArray()
 
 	private static void printUniqueNumber(int[] array) {
-		// array배열에서 중복되지 않은 유일한 숫자만을 출력하는 프로그램을 구현하시오.		
-		
-			
+		// array배열에서 중복되지 않은 유일한 숫자만을 출력하는 프로그램을 구현하시오.
+		boolean chk = false;
+		for (int i = 0; i < array.length; i++) {
+			chk = false;
+			for (int j = 0; j < array.length; j++) {
+				if (i != j && array[i] == array[j]) {
+					chk = true;
+					break;
+				}
+			} // end for j
+			if (chk == false)
+				System.out.printf("%5d", array[i]);
+		} // end for i
 
 	}// end printUniqueNumber()
 }// end Prob04
-
-
-
-
-
-
-
-
-
-
-
