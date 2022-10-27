fb0333374220735a69e263e36fb1b033e29f8c85
eoooomji <akcks101@gmail.com>
2022-10-27 18:20:07 +0900

    update | 20221027

--- src/ncs/test12/PropTest.java
+++ src/ncs/test12/PropTest.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
 public class PropTest {
@@ -13,37 +14,54 @@ public class PropTest {
 	public static void main(String[] args) {
 		Properties prop = new Properties();
 		// 명시한 사용 데이터를 prop에 기록한다.
-		try {
-			FileWriter file = new FileWriter("./src/ncs/test12/data.xml");
+		prop.setProperty("1", "apple,1200,3");
+		prop.setProperty("2", "banana,2500,2");
+		prop.setProperty("3", "grape,4500,5");
+		prop.setProperty("4", "orange,800,10");
+		prop.setProperty("5", "melon,5000,2");
 
-			prop.setProperty("1", "apple,1200,3");
-			prop.setProperty("2", "banana,2500,2");
-			prop.setProperty("3", "grape,4500,5");
-			prop.setProperty("4", "orange,800,10");
-			prop.setProperty("5", "melon,5000,2");
-
-			prop.store(file, "end");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		System.out.println(prop);
 		// fileSave() 메서드를 호출한다.
 		fileSave(prop);
 		// fileOpen() 메서드를 호출한다.
+		fileOpen(prop);
 
 	} // end main()
 
 	// fileSave() 메서드를 구현한다.
 	public static void fileSave(Properties p) {
-		for(int i = 0 ; i < p.size();i++) {
+		try {
+			p.storeToXML(new FileOutputStream("./src/ncs/test12/data.xml"), null);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	} // end fileSave()
 
 	// fileOpen() 메서드를 구현한다.
 	public static void fileOpen(Properties p) {
+		FileInputStream fs = null;
+		Fruit[] fruitArr = new Fruit[p.size()];
 
+		try {
+			fs = new FileInputStream("./src/ncs/test12/data.xml");
+			p.loadFromXML(fs);
+
+			for (int i = 1; i <= p.size(); i++) {
+				String index = String.valueOf(i);
+
+				String[] line = p.getProperty(index).split(",");
+				Fruit ft = new Fruit(line[0], Integer.parseInt(line[1]), Integer.parseInt(line[2]));
+				fruitArr[i - 1] = ft;
+			}
+			for (Fruit ff : fruitArr)
+				System.out.println(ff);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (InvalidPropertiesFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	} // end fileOpen()
 
 } // end class
